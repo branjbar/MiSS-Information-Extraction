881588116f4b4aab3b5365991295799578f042f3
Bijan Ranjbar <bij.ranjbar@gmail.com>
2016-05-19 20:44:13 +0200

    Update import_db.py

--- import_db.py
+++ import_db.py
@@ -23,16 +23,16 @@ def init():
 			word = row[0].replace('"', '').replace("'", '').strip()
 			list_of_last_names_multiple.append(word)
 
-	global list_of_mean_names
-	list_of_mean_names = []
-	with open("db/mean_names.txt", 'r') as f:
+	global list_of_starting_words
+	list_of_starting_words = []
+	with open("db/starting_words.txt", 'r') as f:
 		for line in f:
 			word = line.replace('"', '').replace("'", '').strip()
-			list_of_mean_names.append(word)
+			list_of_starting_words.append(word)
 
 
 if __name__ == "__main__":
 	init()
 	print 'First Names', len(list_of_first_names)
 	print 'Last Names', len(list_of_last_names_multiple)
-	print 'Nasty Names', len(list_of_mean_names)
+	print 'Mean Names', len(list_of_starting_words)
